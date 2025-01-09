7f32da7c101a01a257d057ffb723b353cea49eea
LuisBeer <luis.bierbrauer@web.de>
2025-01-09 14:51:39 +0100

    Augabe 3 aber fehlen noch die construct aufgaben

--- source/quadtree/quadtree.cpp
+++ source/quadtree/quadtree.cpp
@@ -5,7 +5,7 @@
 #include <stdexcept>
 #include <omp.h>
 
-Quadtree::Quadtree(Universe& universe, BoundingBox bounding_box, std::int8_t construct_mode){
+Quadtree::Quadtree(Universe& universe, BoundingBox bounding_box, std::int8_t construct_mode) {
     root = new QuadtreeNode(bounding_box);
 	std::vector<std::int32_t> vec;
     switch (construct_mode) {
@@ -20,38 +20,39 @@ Quadtree::Quadtree(Universe& universe, BoundingBox bounding_box, std::int8_t con
         break;
     default:
         throw std::invalid_argument("Invalid construct_mode");
+    }
 }
 
-Quadtree::~Quadtree(){
+Quadtree::~Quadtree() {
   delete root;
   root = nullptr;
 }
 
-void Quadtree::calculate_cumulative_masses(){
+void Quadtree::calculate_cumulative_masses() {
     root->calculate_node_cumulative_mass();
 }
 
-void Quadtree::calculate_center_of_mass(){
+void Quadtree::calculate_center_of_mass() {
     root->calculate_node_center_of_mass();
 }
 
 
-std::vector<QuadtreeNode*> Quadtree::construct(Universe& universe, BoundingBox BB, std::vector<std::int32_t> body_indices){
+std::vector<QuadtreeNode*> Quadtree::construct(Universe& universe, BoundingBox BB, std::vector<std::int32_t> body_indices) {
 
     //TODO implementieren 3d
 	return std::vector<QuadtreeNode*>();
 
 }
 
-std::vector<QuadtreeNode*> Quadtree::construct_task(Universe& universe, BoundingBox BB, std::vector<std::int32_t> body_indices){
+std::vector<QuadtreeNode*> Quadtree::construct_task(Universe& universe, BoundingBox BB, std::vector<std::int32_t> body_indices) {
     return std::vector<QuadtreeNode*>();
 }
 
-std::vector<QuadtreeNode*> Quadtree::construct_task_with_cutoff(Universe& universe, BoundingBox& BB, std::vector<std::int32_t>& body_indices){
+std::vector<QuadtreeNode*> Quadtree::construct_task_with_cutoff(Universe& universe, BoundingBox& BB, std::vector<std::int32_t>& body_indices) {
     return std::vector<QuadtreeNode*>();
 }
 
-std::vector<BoundingBox> Quadtree::get_bounding_boxes(QuadtreeNode* qtn){
+std::vector<BoundingBox> Quadtree::get_bounding_boxes(QuadtreeNode* qtn) {
     // traverse quadtree and collect bounding boxes
     std::vector<BoundingBox> result;
     // collect bounding boxes from children
