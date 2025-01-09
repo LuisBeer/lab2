f3b7d37fae9700e9bf253afc8200a6d45529c657
LuisBeer <luis.bierbrauer@web.de>
2025-01-09 12:15:34 +0100

    Augabe 3 aber fehlen noch die construct aufgaben

--- source/quadtree/quadtree.cpp
+++ source/quadtree/quadtree.cpp
@@ -1,5 +1,4 @@
 #include "quadtree.h"
-
 #include "quadtreeNode.h"
 #include <set>
 #include <algorithm>
@@ -7,9 +6,26 @@
 #include <omp.h>
 
 Quadtree::Quadtree(Universe& universe, BoundingBox bounding_box, std::int8_t construct_mode){
+    root = new QuadtreeNode(bounding_box);
+
+    switch (construct_mode) {
+        case 0:
+            construct(universe, bounding_box, {}); // Implementiere seriellen Aufbau
+        break;
+        case 1:
+            construct_task(universe, bounding_box, {}); // Parallel ohne Cutoff
+        break;
+        case 2:
+            construct_task_with_cutoff(universe, bounding_box, {}); // Parallel mit Cutoff
+        break;
+        default:
+            throw std::invalid_argument("Invalid construct_mode");
+    }
 }
 
 Quadtree::~Quadtree(){
+  delete root;
+  root = nullptr;
 }
 
 void Quadtree::calculate_cumulative_masses(){
@@ -22,6 +38,53 @@ void Quadtree::calculate_center_of_mass(){
 
 
 std::vector<QuadtreeNode*> Quadtree::construct(Universe& universe, BoundingBox BB, std::vector<std::int32_t> body_indices){
+
+    //TODO implementieren 3d
+
+	 // Erstelle den Wurzelknoten des Quadtrees
+  QuadtreeNode* root = new QuadtreeNode();
+  root->bounding_box = BB;
+  root->center_of_mass_ready = false;
+  root->cumulative_mass_ready = false;
+  root->body_identifier = -1;
+
+  // Teile den Raum des Universums in vier Subquadranten auf
+  std::vector<QuadtreeNode*> subquadranten;
+  for (int i = 0; i < 4; i++) {
+    BoundingBox sub_BB = BB.getSubBoundingBox(i);
+    std::vector<int> sub_body_indices;
+
+    // Finde die Körper, die in dem aktuellen Subquadranten liegen
+    for (int j = 0; j < body_indices.size(); j++) {
+      int body_index = body_indices[j];
+      if (sub_BB.contains(universe.positions[body_index])) {
+        sub_body_indices.push_back(body_index);
+      }
+    }
+
+    // Erstelle einen neuen Knoten für den Subquadranten
+    QuadtreeNode* sub_node = new QuadtreeNode();
+    sub_node->bounding_box = sub_BB;
+    sub_node->center_of_mass_ready = false;
+    sub_node->cumulative_mass_ready = false;
+    sub_node->body_identifier = -1;
+
+    // Wenn es sich um einen Blattknoten handelt, setze die entsprechenden Werte
+    if (sub_body_indices.size() == 1) {
+      sub_node->body_identifier = sub_body_indices[0];
+      sub_node->center_of_mass = universe.positions[sub_body_indices[0]];
+      sub_node->cumulative_mass = universe.weights[sub_body_indices[0]];
+      sub_node->center_of_mass_ready = true;
+      sub_node->cumulative_mass_ready = true;
+    }
+
+    // Füge den Subquadranten zum Vektor hinzu
+    subquadranten.push_back(sub_node);
+  }
+
+  // Setze die direkten Subquadranten von root
+  root->children = subquadranten;
+
     return std::vector<QuadtreeNode*>();
 }
 
--- source/quadtree/quadtreeNode.cpp
+++ source/quadtree/quadtreeNode.cpp
@@ -4,15 +4,54 @@
 
 
 double QuadtreeNode::calculate_node_cumulative_mass(){
-    return 0.0;
+    if (children.empty()) { // Blattknoten
+        cumulative_mass_ready = true;
+        return cumulative_mass; // Rückgabe der gespeicherten Masse
+    }
+
+    cumulative_mass = 0.0; // Reset
+    for (auto* child : children) {
+        cumulative_mass += child->calculate_node_cumulative_mass(); // Rekursiver Aufruf
+    }
+    cumulative_mass_ready = true;
+    return cumulative_mass;
 }
 
 QuadtreeNode::QuadtreeNode(BoundingBox arg_bounding_box){
+        : bounding_box(arg_bounding_box), body_identifier(-1), cumulative_mass(0.0),
+            center_of_mass_ready(false), cumulative_mass_ready(false) {
+            // Standardinitialisierung der Felder
 }
 
 QuadtreeNode::~QuadtreeNode(){
+        // Speicher der Unterknoten freigeben
+        for (auto* child : children) {
+            delete child; // Dynamischen Speicher freigeben
+        }
+        children.clear(); // Sicherstellen, dass keine ungültigen Zeiger verbleiben
 }
 
 Vector2d<double> QuadtreeNode::calculate_node_center_of_mass(){
-    return Vector2d<double>(0.0, 0.0);
+            if (children.empty()) { // Blattknoten
+                center_of_mass_ready = true;
+                return center_of_mass; // Rückgabe der gespeicherten Position
+            }
+
+            Vector2d<double> total_center(0.0, 0.0);
+            cumulative_mass = 0.0;
+
+            for (auto* child : children) {
+                double child_mass = child->calculate_node_cumulative_mass();
+                Vector2d<double> child_center = child->calculate_node_center_of_mass();
+
+                total_center += child_center * child_mass;
+                cumulative_mass += child_mass;
+            }
+
+            if (cumulative_mass > 0) {
+                center_of_mass = total_center / cumulative_mass; // Gewichteter Mittelwert
+            }
+
+            center_of_mass_ready = true;
+            return center_of_mass;
 }
