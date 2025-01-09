8ec3e1e9f02c377760eeeedb5ce36cce909f44f9
LuisBeer <luis.bierbrauer@web.de>
2025-01-09 23:30:29 +0100

    chat

--- source/quadtree/quadtree.cpp
+++ source/quadtree/quadtree.cpp
@@ -39,7 +39,48 @@ void Quadtree::calculate_center_of_mass() {
 
 std::vector<QuadtreeNode*> Quadtree::construct(Universe& universe, BoundingBox BB, std::vector<std::int32_t> body_indices) {
 
-    //TODO implementieren 3d
+    // Basisfall: Kein Körper im Bereich
+    if (body_indices.empty()) {
+        return {};
+    }
+
+    // Basisfall: Genau ein Körper -> Blattknoten
+    if (body_indices.size() == 1) {
+        QuadtreeNode* leaf_node = new QuadtreeNode(BB);
+        int body_index = body_indices[0];
+        leaf_node->body_identifier = body_index;
+        leaf_node->cumulative_mass = universe.weights[body_index];
+        leaf_node->center_of_mass = universe.positions[body_index];
+        leaf_node->cumulative_mass_ready = true;
+        leaf_node->center_of_mass_ready = true;
+        return {leaf_node}; // Direkt zurückgeben
+    }
+
+    // Unterteile BoundingBox in 4 Subquadranten
+    std::vector<BoundingBox> sub_boxes = BB.subdivide();
+    std::vector<QuadtreeNode*> children_nodes;
+
+    for (const auto& sub_box : sub_boxes) {
+        std::vector<int32_t> sub_indices;
+
+        // Verteile die Körper auf die Subquadranten
+        for (int32_t body_index : body_indices) {
+            if (sub_box.contains(universe.positions[body_index])) {
+                sub_indices.push_back(body_index);
+            }
+        }
+
+        // Rekursiv weiter für jeden Subquadranten
+        if (!sub_indices.empty()) {
+            QuadtreeNode* child_node = new QuadtreeNode(sub_box);
+            auto sub_children = construct(universe, sub_box, sub_indices);
+            child_node->children = sub_children;
+            children_nodes.push_back(child_node);
+        }
+    }
+
+    return children_nodes; // Rückgabe aller Subknoten
+
 	return std::vector<QuadtreeNode*>();
 
 }
